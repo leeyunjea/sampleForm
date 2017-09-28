34c33b8a6dfb31e6275713b1aaf0208e0653733a
leeyunjea <yunjea0312@naver.com>
2017-09-28 21:21:20 +0900

    web_framework first homework

---
+++ src/controllar/DoLogin.java
@@ -0,0 +1,38 @@
+package controllar;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet(name = "doLogin", urlPatterns = { "/doLogin" })
+public class DoLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		String customerId = request.getParameter("customerId");
+		
+		CustomerService customerService = new CustomerService();
+		
+	}
+
+}
---
+++ src/service/CustomerService.java
@@ -0,0 +1,39 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Customer;
+
+public class CustomerService {
+	
+	private Map<String, Customer> customerMap;
+	
+	public CustomerService() {
+		customerMap = new HashMap<String, Customer>();
+		
+		addCustomer(new Customer("id001", "1234", "이윤재"));
+		addCustomer(new Customer("id002", "1234", "이예지"));
+		addCustomer(new Customer("id002", "1234", "김하늘"));
+		addCustomer(new Customer("id002", "1234", "홍성문"));
+		addCustomer(new Customer("id002", "1234", "최원균"));
+	}
+	
+	private void addCustomer(Customer customer) {
+		customerMap.put(customer.getId(), customer);
+	}
+	
+	public Customer findCustomer(String id) {
+		if(id != null)
+			return (customerMap.get(id.toLowerCase()));
+		else
+			return null;
+	}
+	public List<Customer> getAllCustomers() {
+		List<Customer> customerList = new ArrayList<Customer>(customerMap.values());
+		return customerList;
+	}
+
+}
